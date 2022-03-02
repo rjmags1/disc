1fed367bd0b292ec4c8c65c824cc45d04d1f1e8e
RJ Maganini <maganinirj@gmail.com>
2022-03-01 19:29:33 -0500

    change spec per prev LogoLink commit

--- __tests__/layout/LogoLink.spec.js
+++ __tests__/layout/LogoLink.spec.js
@@ -3,11 +3,10 @@ import { render, screen } from '@testing-library/react'
 import LogoLink from '../../components/layout/LogoLink'
 
 describe('LogoLink', () => {
-    test('renders a div containing img wrapped in anchor', () => {
+    test('renders a div containing anchor with text disc', () => {
         render(<LogoLink />)
         expect(screen.getByTestId("logo-link-container")).toBeInTheDocument()
-        expect(screen.getByRole("link")).toBeInTheDocument()
-        expect(screen.getByRole("img")).toBeInTheDocument()
+        expect(screen.getByText("disc")).toBeInTheDocument()
     })
 
     test('links to homepage', () => {
