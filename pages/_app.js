43cbc67975f98d40b119f20f64f68852a30a155b
RJ Maganini <maganinirj@gmail.com>
2022-03-01 20:30:34 -0500

    dont pass pageName to React.Fragment

--- pages/_app.js
+++ pages/_app.js
@@ -6,12 +6,12 @@ import Layout from '../components/layout/Layout'
 function MyApp({ Component, pageProps, ...appProps }) {
     const pageName = Component.name
     const renderWithLayout = !appProps.router.pathname.match(/login/g)
-    const LayoutComponent = renderWithLayout ? Layout : React.Fragment
-    return (
-        <LayoutComponent pageName={ pageName }>
+    return renderWithLayout ? (
+        <Layout pageName={ pageName } >
             <Component {...pageProps} />
-        </LayoutComponent>
-    )
+        </Layout>
+    ) :
+    ( <Component {...pageProps} /> )
 }
 
 export default MyApp
