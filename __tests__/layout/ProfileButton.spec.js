139d1c792e90e48719f549bf1d1a99219fb6e551
RJ Maganini <maganinirj@gmail.com>
2022-03-01 19:43:24 -0500

    profile button dropdown fxn

--- __tests__/layout/ProfileButton.spec.js
+++ __tests__/layout/ProfileButton.spec.js
@@ -1,12 +1,33 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import ProfileButton from '../../components/layout/ProfileButton'
 
 describe('ProfileButton', () => {
-    test('renders div containing image button', () => {
+    test('renders div containing profile image and dropdown arrow', () => {
         render(<ProfileButton />)
         expect(screen.getByTestId("profile-button-container")).toBeInTheDocument()
-        expect(screen.getByRole("button")).toBeInTheDocument()
-        expect(screen.getByRole("button")).toHaveAttribute("type", "image")
+        expect(screen.getAllByRole("img").length).toEqual(2)
+    })
+    
+    test('on click dropdown menu appears and arrow flips', () => {
+        render(<ProfileButton />)
+        expect(screen.queryByTestId("profile-dropdown-container")).toBeNull()
+        expect(screen.getByTestId("profile-button-arrow").className).toEqual("")
+        userEvent.click(screen.getByTestId("profile-button-container"))
+        expect(screen.getByTestId("profile-dropdown-container")).toBeInTheDocument()
+        expect(screen.getByTestId("profile-button-arrow").className).toMatch(/rotate-180/g)
+        userEvent.click(screen.getByTestId("profile-button-container"))
+    })
+
+    test('if dropdown showing, another click on button removes dropdown', () => {
+        render(<ProfileButton />)
+        userEvent.click(screen.getByTestId("profile-button-container"))
+        expect(screen.getByTestId("profile-dropdown-container")).toBeInTheDocument()
+        userEvent.click(screen.getByTestId("profile-button-container"))
+        expect(screen.queryByTestId("profile-dropdown-container")).toBeNull()
+    })
+
+    test('if dropdown showing, click outside removes dropdown', () => {
     })
 })
--- components/layout/ProfileButton.js
+++ components/layout/ProfileButton.js
@@ -1,8 +1,21 @@
+import { useState } from 'react'
+
+import Image from 'next/image'
+import ProfileDropdownMenu from './ProfileDropdownMenu'
+
 function ProfileButton() {
+    const [clicked, setClicked] = useState(false)
+
     return (
-        <div data-testid="profile-button-container" className="p-1 sm:p-3 mt-1">
-            <input type="image" src="/profile-button-img.png" 
-                className="rounded-full" width="30" height="30"/>
+        <div data-testid="profile-button-container" 
+            className="px-1 sm:p-3 flex flex-col items-center"
+            onClick={() => { setClicked(!clicked)}}>
+            <Image src="/profile-button-img.png" height="25" width="25"
+                className="py-1 rounded-full"/>
+            <Image src="/sort-down.png" height="10" width="10"
+                data-testid="profile-button-arrow"
+                className={ clicked ? "rotate-180" : "" }/>
+            { clicked && <ProfileDropdownMenu /> }
         </div>
     )
 }
