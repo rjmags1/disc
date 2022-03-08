966f208f8d5e60da07c42275c188703a2b4365d9
RJ Maganini <maganinirj@gmail.com>
2022-03-08 13:14:43 -0500

    use state for dummy name, improve styles

--- components/settings/account/AccountSettingsMenu.js
+++ components/settings/account/AccountSettingsMenu.js
@@ -5,17 +5,17 @@ import AccountEmailSection from './AccountEmailSection'
 import AccountPasswordSection from './AccountPasswordSection'
 
 function AccountSettingsMenu() {
+    const [name, setName] = useState("")
     const [emails, setEmails] = useState([])
     const [primaryEmail, setPrimaryEmail] = useState("")
     const [avatarSrc, setAvatarSrc] = useState("/profile-button-img.png")
 
-    let name
     useEffect(() => {
         // client side (useEffect) fetch user account settings
         // then rel. setState calls in useEffect after fetch
         // dummy for now
-        name = "dummy-name"
         //setAvatarSrc("/dummy-src")
+        setName("dummy-name")
         setPrimaryEmail("dummy-email-0")
         setEmails(["dummy-email-0", "dummy-email-1", "dummy-email-2"])
     }, [])
@@ -26,9 +26,11 @@ function AccountSettingsMenu() {
 
     return  (
         <div data-testid="account-menu-container"
-            className="flex-auto min-w-fit">
-            <ProfileCard name={ name } email={ primaryEmail } avatarSrc={ avatarSrc } handleNewSrc={ setAvatarSrc }/>
-            <AccountEmailSection primary={ primaryEmail } emails={ emails } updateDisplayedEmails={ showNewEmail } />
+            className="bg-zinc-900 text-white h-full p-6 flex-auto w-3/4">
+            <ProfileCard name={ name } email={ primaryEmail }
+                avatarSrc={ avatarSrc } handleNewSrc={ setAvatarSrc }/>
+            <AccountEmailSection primary={ primaryEmail }
+                emails={ emails } updateDisplayedEmails={ showNewEmail } />
             <AccountPasswordSection />
         </div>
     )
