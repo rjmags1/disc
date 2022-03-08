5aa127033ebebc3272399c4554ccca9b7e948d72
RJ Maganini <maganinirj@gmail.com>
2022-03-08 16:28:23 -0500

    clarify reset password functionality to user

--- components/settings/account/AccountPasswordSection.js
+++ components/settings/account/AccountPasswordSection.js
@@ -7,12 +7,15 @@ function AccountPasswordSection() {
         <div data-testid="account-password-section-container"
             className="mt-10">
             <div className="ml-4">
-                <h2 className="text-2xl">Password</h2>
+                <h2 className="text-2xl mb-1">Password</h2>
                 <button onClick={ handleClick }
                     className="mt-2 bg-red-600 border border-white rounded
                         p-1 hover:cursor-pointer hover:bg-black px-4">
                     Reset Password
                 </button>
+                <p className="text-sm mt-2">
+                    Click to have a reset password email sent to your primary email.
+                </p>
             </div>
         </div>
     )
