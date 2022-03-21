e0fd892f2a2c33c6eca15fe5a55c67f88514b438
RJ Maganini <maganinirj@gmail.com>
2022-03-21 18:35:41 -0400

    user api route

---
+++ pages/api/user.js
@@ -0,0 +1,25 @@
+import { withIronSessionApiRoute } from 'iron-session/next'
+import { sessionOptions } from '../../lib/session'
+
+export default withIronSessionApiRoute(async function (req, resp) {
+    if (req.method !== 'GET') {
+        resp.status(405).json({ message: "invalid method" })
+        return
+    }
+    if (req.session.user) {
+        resp.json({
+            ...req.session.user
+        })
+    }
+    else {
+        resp.json({
+            authenticated: false,
+            userid: "",
+            fname: "",
+            lname: "",
+            email: "",
+            avatarurl: ""
+        })
+    }
+}, sessionOptions)
+
