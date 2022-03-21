1d825c7ddb931d48c43815d08a101b273145825d
RJ Maganini <maganinirj@gmail.com>
2022-03-21 17:23:37 -0400

    logout api route

---
+++ pages/api/logout.js
@@ -0,0 +1,22 @@
+import { withIronSessionApiRoute } from 'iron-session/next'
+import { sessionOptions } from '../../lib/session'
+
+export default withIronSessionApiRoute(logoutRoute, sessionOptions)
+
+async function logoutRoute(req, resp) {
+    if (req.method !== 'POST') {
+        resp.status(405).json({ message: "invalid method" })
+        return
+    }
+    if (!req.session.user) {
+        resp.status(400).json({ message: "not signed in" })
+    }
+    req.session.destroy()
+    resp.status(200).json({
+        authenticated: false,
+        userId: "",
+        username: "",
+        email: "",
+        avatarUrl: ""
+    })
+}
