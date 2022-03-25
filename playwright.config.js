d01bbbf95a5d9544d18c69d1c0bf2d9b33ec8a18
RJ Maganini <maganinirj@gmail.com>
2022-03-25 16:13:42 -0400

    playwright setup

---
+++ playwright.config.js
@@ -0,0 +1,43 @@
+const { devices } =  require('@playwright/test')
+const path = require('path')
+
+const config = {
+    timeout: 30 * 1000,
+    retries: 2,
+    testDir: './e2e',
+    outputDir: './e2e/results/',
+
+    webServer: {
+        command: 'npm run dev',
+        port: 3000,
+        timeout: 120 * 1000,
+        reuseExistingServer: !process.env.CI,
+    },
+
+    use: { trace: 'retry-with-trace', },
+
+    projects: [
+    {
+        name: 'Desktop Chrome',
+        use: { ...devices['Desktop Chrome'], },
+    },
+    {
+        name: 'Desktop Firefox',
+        use: { ...devices['Desktop Firefox'], },
+    },
+    {
+        name: 'Desktop Safari',
+        use: { ...devices['Desktop Safari'], },
+    },
+    {
+        name: 'Mobile Chrome',
+        use: { ...devices['Pixel 5'], },
+    },
+    {
+        name: 'Mobile Safari',
+        use: devices['iPhone 12'],
+    },
+    ],
+}
+
+module.exports = config
