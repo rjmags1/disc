ca788e9d9abdbe39af395de2aa2d03da5f14ff51
RJ Maganini <maganinirj@gmail.com>
2022-03-25 15:02:52 -0400

    fix login data list test

--- __tests__/login/LoginDatalist.spec.js
+++ __tests__/login/LoginDatalist.spec.js
@@ -8,13 +8,14 @@ describe('LoginDatalist', () => {
         label: "test-label",
         datalistId: "test",
         inputId: "test",
-        optionValues: ["Hogwarts", "Krusty Krab", "McDonalds"],
         containerTestId: "datalist-container-test",
         datalistTestId: "datalist-datalist-test"
     }
 
+    const optionValues = ["Hogwarts", "Krusty Krab", "McDonalds"]
+
     test('renders a div containing label, input, datalist', () => {
-        render(<LoginDatalist info={ datalistInfo } />)
+        render(<LoginDatalist info={ datalistInfo } optionValues={ optionValues } />)
         expect(screen.getByTestId("datalist-container-test")).toBeInTheDocument()
         expect(screen.getByLabelText("test-label")).toBeInTheDocument()
         expect(screen.getByRole("combobox")).toBeRequired()
@@ -22,19 +23,19 @@ describe('LoginDatalist', () => {
     })
 
     test('renders all options and displays empty str on load', () => {
-        render(<LoginDatalist info={ datalistInfo } />)
+        render(<LoginDatalist info={ datalistInfo } optionValues={ optionValues } />)
         expect(screen.getByTestId("datalist-datalist-test").children.length).toEqual(3)
         expect(screen.getByRole("combobox")).toHaveValue("")
     })
 
     test('required input', () => {
-        render(<LoginDatalist info={ datalistInfo } />)
+        render(<LoginDatalist info={ datalistInfo }  optionValues={ optionValues } />)
         expect(screen.getByRole("combobox")).toBeRequired()
     })
 
     test('handleChange callback on change', () => {
         const mockHandleChange = jest.fn()
-        render(<LoginDatalist info={ datalistInfo } handleChange={ mockHandleChange } />)
+        render(<LoginDatalist info={ datalistInfo } handleChange={ mockHandleChange }  optionValues={ optionValues } />)
         userEvent.type(screen.getByRole("combobox"), "four")
         expect(mockHandleChange).toHaveBeenCalledTimes(4)
     })
