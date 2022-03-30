cf47d727e2604d3c228dc852a3afc7d97cdf6ebd
RJ Maganini <maganinirj@gmail.com>
2022-03-30 17:57:45 -0400

    improve readability validation + spec

--- __tests__/lib/validation.spec.js
+++ __tests__/lib/validation.spec.js
@@ -5,32 +5,45 @@ const MAX_EMAIL_PREFIX_LENGTH = 64
 
 describe('email validation', () => {
     test('reject @ in server name', () => {
-        expect(validEmail("testuser@valid-subdom@in.@invalid-domain")).toEqual(false)
+        expect(validEmail("testuser@valid-subdom@in.@invalid-domain")).
+            toEqual(false)
     })
+
 
     test('reject no @ in email', () => {
         expect(validEmail("testuserattest-domain.com")).toEqual(false)
     })
 
+
     test('reject @@ in email', () => {
         expect(validEmail("testuser@@test-domain.com")).toEqual(false)
     })
+
 
     test('reject empty email prefix', () => {
         expect(validEmail("@test.com")).toEqual(false)
     })
 
+
     test('reject domain <2 chars', () => {
         expect(validEmail("test@valid-subdomain.t")).toEqual(false)
     })
 
+
     test('reject excessive prefix length', () => {
-        const excessivePrefixEmail = "prefix".repeat(Math.floor(MAX_EMAIL_PREFIX_LENGTH / 6) + 1) + "@validSuffix.com"
+        const excessivePrefix = "prefix".repeat(
+            Math.floor(MAX_EMAIL_PREFIX_LENGTH / 6) + 1)
+        const excessivePrefixEmail = excessivePrefix + "@validSuffix.com"
+
         expect(validEmail(excessivePrefixEmail)).toEqual(false)
     })
 
+
     test('reject excessive suffix length', () => {
-        const excessiveSuffixEmail = "validPrefix@" + "suffix".repeat(Math.floor(MAX_EMAIL_SUFFIX_LENGTH / 6) + 1)
+        const excessiveSuffix = "suffix".repeat(
+            Math.floor(MAX_EMAIL_SUFFIX_LENGTH / 6) + 1)
+        const excessiveSuffixEmail = "validPrefix@" + excessiveSuffix
+
         expect(validEmail(excessiveSuffixEmail)).toEqual(false)
     })
 
@@ -39,20 +52,24 @@ describe('email validation', () => {
     })
 })
 
+
 describe('password validation', () => {
     test('reject invalid password length', () => {
         expect(validPassword("2shortt")).toEqual(false)
     })
+
 
     test('dont reject valid password', () => {
         expect(validPassword("longenuff")).toEqual(true)
     })
 })
 
+
 describe('org validation', () => {
     test('reject non alpha org input', () => {
         expect(validOrg("asdf1234!@#$")).toEqual(false)
     })
+
 
     test('accept alpha org input', () => {
         expect(validOrg("asdfASDF")).toEqual(true)
