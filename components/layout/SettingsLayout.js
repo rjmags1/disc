5bfa5d9069bacd54fbeb3f263e05c650c53bd349
RJ Maganini <maganinirj@gmail.com>
2022-04-12 09:23:18 -0400

    add scrollbar auto to account menu container if overflow

--- components/layout/SettingsLayout.js
+++ components/layout/SettingsLayout.js
@@ -3,7 +3,7 @@ import SettingsMenuPane from '../settings/SettingsMenuPane'
 function SettingsLayout({ children }) {
     return (
         <div data-testid="settings-container"
-            className="flex items-start h-full">
+            className="flex items-start h-full pb-10">
             <SettingsMenuPane />
             { children }
         </div>
