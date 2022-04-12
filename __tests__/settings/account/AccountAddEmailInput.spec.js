0f37d4236e546c055a890075eaef9c8d369b4327
RJ Maganini <maganinirj@gmail.com>
2022-04-12 15:28:15 -0400

    review account settings component unit test, removals sufficiently covered in e2e

---
+++ __tests__/settings/account/AccountAddEmailInput.spec.js
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+
+import AccountAddEmailInput from '../../../components/settings/account/AccountAddEmailInput'
+
+describe('AccountAddEmailInput', () => {
+    test('renders new email input with add email address label', () => {
+        render(<AccountAddEmailInput />)
+
+        expect(screen.getByRole('textbox')).toBeInTheDocument()
+
+        expect(screen.getByText(/add email address/gi)).
+            toBeInTheDocument()
+    })
+
+    test('renders button to submit new email with add label', () => {
+        render(<AccountAddEmailInput />)
+
+        expect(screen.getByRole('button')).toBeInTheDocument()
+
+        expect(screen.getByText(/^add$/i)).toBeInTheDocument()
+    })
+})
