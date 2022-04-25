e52056312b06a69053ada26673ac4b7e34ccf646
RJ Maganini <maganinirj@gmail.com>
2022-04-25 09:30:08 -0400

    category pane: use pixels instead of percent for desired responsive design

--- pages/[courseId]/discussion.js
+++ pages/[courseId]/discussion.js
@@ -2,51 +2,46 @@ import Layout from '../../components/layout/Layout'
 import { useState, useRef, useEffect } from 'react'
 
 function Discussion() {
+    const MAX_CAT_PANE_WIDTH = 400
     const [catPaneWidth, setCatPaneWidth] = useState(null)
     const catPane = useRef(null)
-
-    const pixelsToPercentStr = (pixels) => {
-            const inPercent = (pixels / document.body.clientWidth) * 100
-            return `${inPercent}%`
-    }
 
     useEffect(() => {
         if (!catPane.current || catPaneWidth === null) return
 
-        const maxWidthInPixels = Math.floor(document.body.clientWidth / 6)
-        if (maxWidthInPixels < catPaneWidth) return
+        if (MAX_CAT_PANE_WIDTH < catPaneWidth) return
 
-        const newActualCatPaneWidth = pixelsToPercentStr(catPaneWidth)
-        catPane.current.style.width = newActualCatPaneWidth
+        catPane.current.style.width = `${catPaneWidth}px`
     }, [catPaneWidth])
 
-    const handleMouseDown = () => {
-        document.addEventListener('mousemove', handleDrag)
-        document.addEventListener('mouseup', handleMouseUp)
+    const handleLeftDividerMouseDown = () => {
+        document.addEventListener('mousemove', handleLeftDividerDrag)
+        document.addEventListener('mouseup', handleLeftDividerMouseUp)
     }
 
-    const handleDrag =  (event) => {
+    const handleLeftDividerDrag = (event) => {
         const newWidthInPixels = event.clientX
         setCatPaneWidth(newWidthInPixels)
     }
 
-    const handleMouseUp = () => {
-        document.removeEventListener('mousemove', handleDrag)
-        document.removeEventListener('mouseup', handleMouseUp)
+    const handleLeftDividerMouseUp = () => {
+        document.removeEventListener('mousemove', handleLeftDividerDrag)
+        document.removeEventListener('mouseup', handleLeftDividerMouseUp)
     }
 
     return (
         <div data-testid="discussion-container" className="flex h-full">
             <div data-testid="category-pane-container" ref={ catPane }
-                className="hidden lg:flex bg-zinc-700 max-w-[1/6] 
-                            text-white w-[12%] justify-between overflow-hidden">
-                <div>
+                className="hidden lg:flex bg-zinc-700 text-white w-64
+                    justify-between overflow-hidden">
+                <div data-testid="category-headers-container">
                     <h3>category1</h3>
                     <h3>category2</h3>
                     <h3>category3</h3>
                 </div>
                 <div className="w-1 bg-zinc-500 hover:cursor-ew-resize" 
-                    onMouseDown={ handleMouseDown } />
+                    onMouseDown={ handleLeftDividerMouseDown } 
+                    data-testid="left-divider" />
             </div>
             <div className="flex-auto text-white flex" id="posts-section">
                 Post listing and post display
