3a9f92aa07d9b26f1c2f7fd96a6a290e190bef1c
RJ Maganini <maganinirj@gmail.com>
2022-04-26 09:28:58 -0400

    fix initial width due to null initial state

--- components/discussion/CategoryPane.js
+++ components/discussion/CategoryPane.js
@@ -1,16 +1,27 @@
 import { useState, useEffect } from 'react'
+import { useCategories } from '../../lib/hooks'
+import { useRouter } from 'next/router'
+import Loading from '../lib/Loading'
+import Category from './Category'
 
 function CategoryPane({ catPaneRef }) {
     const MAX_CAT_PANE_WIDTH = 400
     const INITIAL_CAT_PANE_WIDTH = 250
+    const router = useRouter()
+    const { courseId } = router.query
 
-    const [catPaneWidth, setCatPaneWidth] = useState(null)
+    const [catPaneWidth, setCatPaneWidth] = useState(INITIAL_CAT_PANE_WIDTH)
 
     useEffect(() => {
         if (!catPaneRef.current) return
 
         catPaneRef.current.style.width = `${ catPaneWidth }px`
     }, [catPaneWidth, catPaneRef])
+
+    const {
+        categories: categoriesInfo,
+        loading: loadingCategories
+    } = useCategories(courseId)
 
     const handleLeftDividerMouseDown = () => {
         document.addEventListener('mousemove', handleLeftDividerDrag)
@@ -25,14 +36,21 @@ function CategoryPane({ catPaneRef }) {
         document.removeEventListener('mouseup', handleLeftDividerMouseUp)
     }
 
+    if (loadingCategories) return <Loading />
+
+    const categories = categoriesInfo.map(
+        categoryInfo => {
+            const { categoryId } = categoryInfo
+            return <Category info={ categoryInfo } key={ categoryId } />
+        }
+    )
+
     return (
         <div data-testid="category-pane-container" ref={ catPaneRef }
             className={ `hidden lg:flex bg-zinc-700 text-white justify-between
                 overflow-hidden w-[${ INITIAL_CAT_PANE_WIDTH }px]` } >
-            <div data-testid="category-headers-container">
-                <h3>category1</h3>
-                <h3>category2</h3>
-                <h3>category3</h3>
+            <div data-testid="category-headers-container" >
+                { categories }
             </div>
             <div className="w-1 bg-zinc-500 hover:cursor-ew-resize"
                 onMouseDown={ handleLeftDividerMouseDown }
