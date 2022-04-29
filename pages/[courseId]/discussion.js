a5143e07e10e9d5c7b32327231eb880492ecd665
RJ Maganini <maganinirj@gmail.com>
2022-04-28 20:56:59 -0400

    get rid of body scrollbar on discussion page

--- pages/[courseId]/discussion.js
+++ pages/[courseId]/discussion.js
@@ -5,12 +5,19 @@ import PostListingsPane from '../../components/discussion/PostListingsPane'
 import Post from '../../components/discussion/Post'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
-import {  useRef } from 'react'
+import { useRef, useLayoutEffect } from 'react'
 import { useUser, useCourse } from '../../lib/hooks'
 
 function Discussion() {
     const router = useRouter()
     const catPaneRef = useRef(null)
+
+    useLayoutEffect(() => {
+        document.body.style.overflow = "hidden"
+        return () => {
+            document.body.style.overflow = ""
+        }
+    }, [])
 
     const {
         user,
