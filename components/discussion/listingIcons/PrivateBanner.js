93c89567a43d2e68c776e969affb56be5ade9280
RJ Maganini <maganinirj@gmail.com>
2022-04-30 17:22:59 -0400

    remove redundant style

--- components/discussion/listingIcons/PrivateBanner.js
+++ components/discussion/listingIcons/PrivateBanner.js
@@ -1,6 +1,6 @@
 function PrivateBanner() {
     return (
-        <span className="font-sans text-[.6rem] leading-3 tracking-tight bg-red-700
+        <span className="text-[.6rem] leading-3 tracking-tight bg-red-700
             border border-white rounded px-1 h-max mr-2">
             PRIVATE
         </span>
--- components/discussion/listingIcons/StaffBanner.js
+++ components/discussion/listingIcons/StaffBanner.js
@@ -1,6 +1,6 @@
 function StaffBanner() {
     return (
-        <span className="font-sans text-[.6rem] leading-3 
+        <span className="text-[.6rem] leading-3 
             tracking-tight bg-sky-700
             border border-white rounded px-1 h-max mr-2">
             STAFF
