34261cb16f9043ecd1868f54eb0035db4ad421ea
RJ Maganini <maganinirj@gmail.com>
2022-04-30 19:21:27 -0400

    show actual number of likes and comments instead of placeholder text

--- components/discussion/listingIcons/Comments.js
+++ components/discussion/listingIcons/Comments.js
@@ -1,11 +1,11 @@
-function Comment() {
+function Comments({ numComments }) {
     return (
         <div className="w-[20px] h-[20px] flex items-center 
             justify-center opacity-50 mr-2">
-            <span className="text-xs mr-1">12</span>
+            <span className="text-xs mr-1">{ numComments }</span>
             <img src="/comment.png" width="12" className="mt-0.5" />
         </div>
     )
 }
 
-export default Comment
+export default Comments
--- components/discussion/listingIcons/Hearts.js
+++ components/discussion/listingIcons/Hearts.js
@@ -1,11 +1,11 @@
-function Heart() {
+function Hearts({ numHearts }) {
     return (
         <div className="mr-4 opacity-50 flex items-center 
             justify-center h-[20px] w-[20px]">
-            <span className="text-xs mr-0.5">12</span>
+            <span className="text-xs mr-0.5">{ numHearts }</span>
             <img src="/heart.png" width="12" className="mt-[2px]"/>
         </div>
     )
 }
 
-export default Heart
+export default Hearts
