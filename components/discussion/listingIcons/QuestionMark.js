a26430078bc1875b6f101b5ffbbe32e9cc82eddf
RJ Maganini <maganinirj@gmail.com>
2022-04-30 14:32:11 -0400

    question mark icon for post info listing

---
+++ components/discussion/listingIcons/QuestionMark.js
@@ -0,0 +1,11 @@
+function QuestionMark() {
+    return (
+        <div className="border border-white rounded-full w-[20px] h-[20px] 
+            flex items-center justify-items-center mr-2">
+            <img src="/question-mark.png" width="16" 
+                style={ { padding: "2px", marginLeft: "1px" } }/>
+        </div>
+    )
+}
+
+export default QuestionMark
