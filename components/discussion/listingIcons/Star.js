130515ca5ca0f63277eaf8e08f67cb4673e1e95b
RJ Maganini <maganinirj@gmail.com>
2022-04-30 15:33:59 -0400

    fix bad css rule, display is flex not grid

--- components/discussion/listingIcons/Star.js
+++ components/discussion/listingIcons/Star.js
@@ -1,7 +1,7 @@
 function Star() {
     return (
         <div className="w-fit h-[20px] flex items-center 
-            justify-items-center mr-2">
+            justify-center mr-2">
             <img src="/star.png" width="15" className="mb-0.5"/>
         </div>
     )
--- components/discussion/listingIcons/Watching.js
+++ components/discussion/listingIcons/Watching.js
@@ -1,7 +1,7 @@
 function Watching() {
     return (
         <div className="w-[20px] h-[20px] mr-2 flex items-center
-            justify-items-center">
+            justify-center">
             <div className="h-[12px]">
                 <img src="/watching.png" width="19" />
             </div>
