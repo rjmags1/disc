72ef2c412e774306615e93f2d22289e237004d03
RJ Maganini <maganinirj@gmail.com>
2022-04-30 15:55:12 -0400

    checkmark icon for post info listings

---
+++ components/discussion/listingIcons/Checkmark.js
@@ -0,0 +1,9 @@
+function Checkmark() {
+    return (
+        <div className="w-fit h-[20px] flex items-center justify-center mr-2">
+            <img src="/checkmark.png" width="15" />
+        </div>
+    )
+}
+
+export default Checkmark
