aa0bb912a7ef283f6884d43a3c454076f4c5c31a
RJ Maganini <maganinirj@gmail.com>
2022-04-30 14:33:06 -0400

    announcement icon for post info listing

---
+++ components/discussion/listingIcons/Announcement.js
@@ -0,0 +1,10 @@
+function Announcement() {
+    return (
+        <div className="w-[20px] h-[20px] mr-2 flex 
+            items-center justify-items-center">
+            <img src="/announcement.png" width="18" />
+        </div>
+    )
+}
+
+export default Announcement
