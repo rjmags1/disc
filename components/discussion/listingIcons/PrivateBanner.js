7ddcd5ea5004440e69d469141b2ada77df320fc5
RJ Maganini <maganinirj@gmail.com>
2022-04-30 17:13:49 -0400

    private and staff-author banners for post info listings

---
+++ components/discussion/listingIcons/PrivateBanner.js
@@ -0,0 +1,10 @@
+function PrivateBanner() {
+    return (
+        <span className="font-sans text-[.6rem] leading-3 tracking-tight bg-red-700
+            border border-white rounded px-1 h-max mr-2">
+            PRIVATE
+        </span>
+    )
+}
+
+export default PrivateBanner
---
+++ components/discussion/listingIcons/StaffBanner.js
@@ -0,0 +1,11 @@
+function StaffBanner() {
+    return (
+        <span className="font-sans text-[.6rem] leading-3 
+            tracking-tight bg-sky-700
+            border border-white rounded px-1 h-max mr-2">
+            STAFF
+        </span>
+    )
+}
+
+export default StaffBanner
