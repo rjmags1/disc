b0b6120d18629cb00cbb2526d1fe72276ab13a46
RJ Maganini <maganinirj@gmail.com>
2022-04-30 16:15:23 -0400

    comment icon for post listings

---
+++ components/discussion/listingIcons/Comment.js
@@ -0,0 +1,11 @@
+function Comment() {
+    return (
+        <div className="w-fit h-[20px] flex items-center 
+            justify-center opacity-50">
+            <span className="text-xs mr-1">12</span>
+            <img src="/comment.png" width="12" className="mt-0.5" />
+        </div>
+    )
+}
+
+export default Comment
