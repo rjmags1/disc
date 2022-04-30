53ae90fd9a42852b536b4cfe9d0a0ff3c779841c
RJ Maganini <maganinirj@gmail.com>
2022-04-30 16:06:41 -0400

    heart icon for post listing

---
+++ components/discussion/listingIcons/Heart.js
@@ -0,0 +1,10 @@
+function Heart() {
+    return (
+        <div className="mr-2 opacity-50 flex items-center justify-center h-[20px]">
+            <span className="text-xs mr-0.5">12</span>
+            <img src="/heart.png" width="12" className="mt-[2px]"/>
+        </div>
+    )
+}
+
+export default Heart
