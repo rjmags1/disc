81998cf508c47f395763c6e609761b75d8888b62
RJ Maganini <maganinirj@gmail.com>
2022-04-30 15:43:18 -0400

    pinned icon for post info listing

---
+++ components/discussion/listingIcons/Pinned.js
@@ -0,0 +1,10 @@
+function Pinned() {
+    return (
+        <div className="-ml-1 mr-1 w-fit h-[20px] flex
+            items-center justify-center">
+            <img src="/pinned.png" width="23" />
+        </div>
+    )
+}
+
+export default Pinned
