df41b1250b118579deb800a38c600f3719f23956
RJ Maganini <maganinirj@gmail.com>
2022-04-30 15:24:51 -0400

    watching icon for post listings

---
+++ components/discussion/listingIcons/Watching.js
@@ -0,0 +1,12 @@
+function Watching() {
+    return (
+        <div className="w-[20px] h-[20px] mr-2 flex items-center
+            justify-items-center">
+            <div className="h-[12px]">
+                <img src="/watching.png" width="19" />
+            </div>
+        </div>
+    )
+}
+
+export default Watching
