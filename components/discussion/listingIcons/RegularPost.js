5128524fbff50462f281772f0d969d0811009069
RJ Maganini <maganinirj@gmail.com>
2022-04-30 16:28:42 -0400

    regular post icon for post info listings

---
+++ components/discussion/listingIcons/RegularPost.js
@@ -0,0 +1,9 @@
+function RegularPost() {
+    return (
+        <div className="h-[20px] w-fit mr-2 flex items-center justify-start">
+            <img width="15" src="/post.png" />
+        </div>
+    )
+}
+
+export default RegularPost
