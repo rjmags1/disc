ccde24069b3051642061a0607eafd3e3d7a84b58
RJ Maganini <maganinirj@gmail.com>
2022-04-30 15:28:37 -0400

    star icon for post listings

---
+++ components/discussion/listingIcons/Star.js
@@ -0,0 +1,10 @@
+function Star() {
+    return (
+        <div className="w-fit h-[20px] flex items-center 
+            justify-items-center mr-2">
+            <img src="/star.png" width="15" className="mb-0.5"/>
+        </div>
+    )
+}
+
+export default Star
