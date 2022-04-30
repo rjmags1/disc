c4fb04e39865111d8fe5e47737892b72939cb6c7
RJ Maganini <maganinirj@gmail.com>
2022-04-30 16:38:33 -0400

    restyle top left post info icon spacing

--- components/discussion/listingIcons/QuestionMark.js
+++ components/discussion/listingIcons/QuestionMark.js
@@ -1,9 +1,9 @@
 function QuestionMark() {
     return (
-        <div className="border border-white rounded-full w-[20px] h-[20px] 
-            flex items-center mr-2">
-            <img src="/question-mark.png" width="16" 
-                style={ { padding: "2px", marginLeft: "1px" } }/>
+        <div className="border border-white rounded-full w-fit h-[18px] 
+            flex items-center justify-start mr-2">
+            <img src="/question-mark.png" width="15" 
+                className="p-[2px]"/>
         </div>
     )
 }
