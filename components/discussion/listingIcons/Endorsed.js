35fd40717f921589788b906eb2e3c6cab351156e
RJ Maganini <maganinirj@gmail.com>
2022-04-30 15:37:35 -0400

    endorsed icon for post listings

---
+++ components/discussion/listingIcons/Endorsed.js
@@ -0,0 +1,10 @@
+function Endorsed() {
+    return (
+        <div className="w-fit h-[20px] flex items-center
+            justify-center mr-2">
+            <img src="/endorsed.png" width="15"/>
+        </div>
+    )
+}
+
+export default Endorsed
