74852c593cff0b0a9e82a7cb27e1d7ae1e625515
RJ Maganini <maganinirj@gmail.com>
2022-04-30 15:35:51 -0400

    center question mark

--- components/discussion/listingIcons/QuestionMark.js
+++ components/discussion/listingIcons/QuestionMark.js
@@ -1,7 +1,7 @@
 function QuestionMark() {
     return (
         <div className="border border-white rounded-full w-[20px] h-[20px] 
-            flex items-center justify-center mr-2">
+            flex items-center mr-2">
             <img src="/question-mark.png" width="16" 
                 style={ { padding: "2px", marginLeft: "1px" } }/>
         </div>
