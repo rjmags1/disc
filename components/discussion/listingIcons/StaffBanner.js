70b965666605323f89eb3556a8a882442756325b
RJ Maganini <maganinirj@gmail.com>
2022-04-30 21:48:38 -0400

    tweak staff banner spacing

--- components/discussion/listingIcons/StaffBanner.js
+++ components/discussion/listingIcons/StaffBanner.js
@@ -2,7 +2,7 @@ function StaffBanner() {
     return (
         <span className="text-[.6rem] leading-3 
             tracking-tight bg-sky-700
-            border border-white rounded px-1 h-max mx-2">
+            border border-white rounded px-1 h-max ml-2">
             STAFF
         </span>
     )
