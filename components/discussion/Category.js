61570b580e9ea1fe5f57cae5c6b79c43b9f600af
RJ Maganini <maganinirj@gmail.com>
2022-05-03 09:19:46 -0400

    inline style passed bullet color

--- components/discussion/Category.js
+++ components/discussion/Category.js
@@ -3,7 +3,6 @@ import { useRef, useEffect, useState } from 'react'
 
 function Category({ info, bulletColor }) {
     const [selected, setSelected] = useState(null)
-    const bulletRef = useRef(null)
     const containerRef = useRef(null)
 
     useEffect(() => {
@@ -15,19 +14,13 @@ function Category({ info, bulletColor }) {
             "#18181b" : ""
     }, [selected, containerRef])
 
-    useEffect(() => {
-        if (!bulletRef.current) return
-
-        bulletRef.current.style.backgroundColor = bulletColor
-    }, [bulletRef])
-
     return (
         <div data-testid="category-header-container" ref={ containerRef }
             className="w-full py-1 font-mono text-sm flex items-center 
                 hover:cursor-pointer px-4"
             onClick={ () => setSelected(!selected) }>
-            <div data-testid="category-bullet" ref={ bulletRef }
-                className="w-[6px] h-[6px] ml-1 mr-2 shrink-0" />
+            <div data-testid="category-bullet" className="w-[6px] h-[6px] ml-1
+                mr-2 shrink-0" style={{ backgroundColor: bulletColor }} />
             <h4 className="shrink-1 grow whitespace-nowrap 
                     overflow-hidden mr-2 text-ellipsis select-none">
                 { info.category }
