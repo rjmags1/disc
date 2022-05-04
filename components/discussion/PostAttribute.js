c26aed08a044ad90f8fcb90211665d7639197656
RJ Maganini <maganinirj@gmail.com>
2022-05-04 16:18:04 -0400

    minor refactor

--- components/discussion/PostAttribute.js
+++ components/discussion/PostAttribute.js
@@ -1,6 +1,8 @@
 function PostAttribute({ attribute, selected, changeAttribute }) {
+
     return (
-        <div className="hover:bg-zinc-500 hover:cursor-pointer flex justify-between px-2" 
+        <div className="hover:bg-zinc-500 hover:cursor-pointer 
+            flex justify-between px-2" 
             onClick={ () => changeAttribute(attribute) } >
             { selected && <span className="pl-1">✓</span> }
             <p className="text-right w-full pr-1" data-testid="post-attribute">
