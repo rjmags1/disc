afa482f8640517fa7a564842e04f7afcce49346e
RJ Maganini <maganinirj@gmail.com>
2022-05-08 16:17:56 -0400

    refactor category pane

--- components/discussion/CategoryPane.js
+++ components/discussion/CategoryPane.js
@@ -9,9 +9,10 @@ function CategoryPane({ catPaneRef, changeCategoryFilter }) {
     const MAX_CAT_PANE_WIDTH = 400
     const INITIAL_CAT_PANE_WIDTH = 250
     const router = useRouter()
-    const { courseId } = router.query
-
+    const { courseId } = router.query    
     const [catPaneWidth, setCatPaneWidth] = useState(INITIAL_CAT_PANE_WIDTH)
+
+    const { course, loading: loadingCourse } = useCourse(courseId)
 
     useLayoutEffect(() => {
         if (!catPaneRef.current) return
@@ -19,8 +20,6 @@ function CategoryPane({ catPaneRef, changeCategoryFilter }) {
 
         catPaneRef.current.style.width = `${ catPaneWidth }px`
     })
-
-    const { course, loading: loadingCourse } = useCourse(courseId)
 
     const handleLeftDividerMouseDown = () => {
         document.body.style.userSelect = "none"
@@ -39,31 +38,34 @@ function CategoryPane({ catPaneRef, changeCategoryFilter }) {
         document.removeEventListener('mouseup', handleLeftDividerMouseUp)
     }
 
-    const categories = !course?.categories ? [] : course.categories.map(
-        (category, i) => {
-            return <Category name={ category } key={ i } 
-                        changeFilter={ changeCategoryFilter }
-                        bulletColor={ RAINBOW_HEX[i % RAINBOW_HEX.length] }/>
-        }
+    const categories = !course?.categories ? [] : (
+        course.categories.map((category, i) => (
+            <Category bulletColor={ RAINBOW_HEX[i % RAINBOW_HEX.length] }
+                changeFilter={ changeCategoryFilter } name={ category } 
+                key={ i } />))
     )
 
     return (
-        <div data-testid="category-pane-container" ref={ catPaneRef }
+        <section data-testid="category-pane-container" ref={ catPaneRef }
             className={ `hidden md:flex bg-zinc-800 text-white justify-between
                w-[180px] flex-none lg:w-[${ INITIAL_CAT_PANE_WIDTH }px]` } >
             <div data-testid="category-headers-container"
                 className="w-full flex flex-col items-center justify-start 
                     overflow-hidden my-2">
-                <h3 className="text-sm font-mono text-gray-400 w-full
-                        text-left px-4">
-                    CATEGORIES
-                </h3>
+                <header className="w-full">
+                    <h3 className="text-sm font-mono text-gray-400 w-full
+                        text-left px-4 truncate">
+                        CATEGORIES
+                    </h3>
+                </header>
+                <ul className="w-full">
                 { loadingCourse ? <Loading /> : categories }
+                </ul>
             </div>
-            <div className="w-1 bg-zinc-500 hover:cursor-ew-resize hidden lg:block"
-                onMouseDown={ handleLeftDividerMouseDown }
+            <div className="w-1 bg-zinc-500 hover:cursor-ew-resize 
+                hidden lg:block" onMouseDown={ handleLeftDividerMouseDown }
                 data-testid="left-divider" />
-        </div>
+        </section>
     )
 }
 
