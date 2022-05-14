20aa475091a0b1486582a23d74d529395d2e132c
RJ Maganini <maganinirj@gmail.com>
2022-05-14 18:10:35 -0400

    basic post content components

--- components/discussion/post/Post.js
+++ components/discussion/post/Post.js
@@ -1,21 +1,37 @@
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { PostContext, TimeContext } from '../../../pages/[courseId]/discussion'
 import Loading from '../../lib/Loading'
 import NoPostSelected from './NoPostSelected'
 import { usePostContent } from '../../../lib/hooks'
+import PostContentSection from './PostContentSection'
 
 function Post() {
     const [apiPage, setApiPage] = useState(1)
+    const [threads, setThreads] = useState([])
     const { currentPost } = useContext(PostContext)
     const initialLoadTime = useContext(TimeContext)
 
-    const postContent = usePostContent(
+
+    const { content, loading: loadingPostContent } = usePostContent(
         currentPost?.postId, currentPost?.authorId, apiPage, initialLoadTime)
+
+    useEffect(() => {
+        if (!content) return
+
+        console.log(content.descendantInfo)
+        //console.log(content.ancestorInfo, content.descendantInfo)
+        //const newThreads = content.ancestorInfo.map()
+        //setThreads([...threads, ...newThreads])
+    }, [content])
 
     return (
         <div data-testid="post-container"
             className="hidden lg:flex w-full flex-auto bg-light-gray">
             { !currentPost && <NoPostSelected /> }
+            { apiPage === 1 && loadingPostContent && !content && <Loading /> }
+            { !!content && apiPage === 1 && 
+            <PostContentSection 
+                content={{ ...currentPost, ...content.postInfo }} /> }
         </div>
     )
 }
---
+++ components/discussion/post/PostContentSection.js
@@ -0,0 +1,44 @@
+import Timestamp from "../postListingsPane/listingIcons/Timestamp"
+
+function PostContentSection({ content }) {
+    // fetch views and add 
+    const views = 100
+    return (
+        <div data-testid="post-content-container" className="w-full py-[7%] px-[12%] font-thin">
+            <header>
+                <h3 data-testid="post-title" className="text-3xl font-medium">
+                    { content.title }
+                </h3>
+                <section className="flex justify-between my-4 h-[55px] whitespace-nowrap" 
+                    data-testid="post-stats-bar">
+                    <div className="flex flex-none pr-4">
+                        <img src={ content.avatarUrl } width="55" className="rounded-full"/>
+                        <div className="flex flex-col justify-center h-full">
+                            <h4 className="ml-2.5 font-normal">{ content.author }</h4>
+                            <h5 className="text-xs py-1">
+                                <Timestamp createdAt={ new Date(content.createdAt) }/>
+                                <span className="-ml-1">in { content.category }</span>
+                            </h5>
+                        </div>
+                    </div>
+                    <div className="flex text-xs h-full overflow-hidden font-normal">
+                        <div className="flex flex-col justify-center items-center mx-2">
+                            <h4 className="text-lg w-full text-center">{ views }</h4><h6>views</h6>
+                        </div>
+                        <div className="flex flex-col justify-center items-center mx-2">
+                            <h4 className="text-lg w-full text-center">{ content.likes }</h4><h6>likes</h6>
+                        </div>
+                        <div className="flex flex-col justify-center items-center mx-2">
+                            <h4 className="text-lg w-full text-center">{ content.comments }</h4><h6>comments</h6>
+                        </div>
+                    </div>
+                </section>
+            </header>
+            <section data-testid="post-display-content" 
+                dangerouslySetInnerHTML={{ __html: content.displayContent }} 
+                className="font-light" />
+        </div>
+    )
+}
+
+export default PostContentSection
