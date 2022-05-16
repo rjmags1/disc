b7d6fbe0f5e4a3487a2b13f3106fc462b854dc3b
RJ Maganini <maganinirj@gmail.com>
2022-05-16 15:26:34 -0400

    delete and endorse buttons

---
+++ components/discussion/post/controlPanelButtons/DeleteButton.js
@@ -0,0 +1,13 @@
+function DeleteButton() {
+    return (
+        <div data-testid="delete-button-container"
+            className="w-full h-[40px]">
+            <button className="w-full h-full bg-red-600 rounded
+                hover:cursor-pointer hover:bg-red-700">
+                Delete
+            </button>
+        </div>
+    )
+}
+
+export default DeleteButton
---
+++ components/discussion/post/controlPanelButtons/EndorseButton.js
@@ -0,0 +1,13 @@
+function EndorseButton() {
+    return (
+        <div data-testid="endorse-button-container"
+            className="w-full h-[40px]">
+            <button className="w-full h-full bg-blue-600 rounded
+                hover:bg-blue-700 hover:cursor-pointer">
+                Endorse
+            </button>
+        </div>
+    )
+}
+
+export default EndorseButton
