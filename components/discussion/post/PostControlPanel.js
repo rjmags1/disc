998b647d1741619d64848c063bc1ebb6bf88de8e
RJ Maganini <maganinirj@gmail.com>
2022-05-17 13:15:37 -0400

    conditional render edit post button

--- components/discussion/post/PostControlPanel.js
+++ components/discussion/post/PostControlPanel.js
@@ -6,20 +6,23 @@ import EndorseButton from './controlPanelButtons/EndorseButton'
 import LikeButton from './controlPanelButtons/LikeButton'
 import StarButton from './controlPanelButtons/StarButton'
 import WatchButton from './controlPanelButtons/WatchButton'
+import EditButton from './controlPanelButtons/EditButton'
 
 function PostControlPanel() {
     const { currentPost } = useContext(PostContext)
 
     const { user } = useUser()
     const userId = user.user_id
+    const userIsAuthor = userId === currentPost.authorId
     const canEndorse = user.is_admin || user.is_staff || user.is_instructor
-    const canDelete = user.is_admin || userId === currentPost.authorId
+    const canDelete = userIsAuthor || user.is_admin 
 
     return (
         <section data-testid="post-control-panel-container" 
             className="w-max flex mb-3 gap-[3%]">
             <LikeButton liked={ currentPost.liked } />
             <WatchButton watched={ currentPost.watched }/>
+            { userIsAuthor && <EditButton /> }
             <StarButton starred={ currentPost.starred } />
             { canEndorse && <EndorseButton endorsed={ currentPost.endorsed } /> }
             { canDelete && !currentPost.deleted && <DeleteButton /> }
---
+++ components/discussion/post/controlPanelButtons/EditButton.js
@@ -0,0 +1,13 @@
+function EditButton() {
+    return (
+        <div data-testid="edit-button-container" className="h-[25px] text-sm" >
+            <button className="flex items-center justify-center rounded
+                h-full bg-purple hover:bg-violet-800 border border-white
+                hover:cursor-pointer px-1.5 w-max" >
+                <span>Edit</span>
+            </button>
+        </div>
+    )
+}
+
+export default EditButton
