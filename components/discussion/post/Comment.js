3aa27bb5eb518a0e844cf61f91f40eb215e0895e
RJ Maganini <maganinirj@gmail.com>
2022-05-18 20:53:50 -0400

    line up endorsed and checkmark comment icon vertically

--- components/discussion/post/Comment.js
+++ components/discussion/post/Comment.js
@@ -35,13 +35,17 @@ const Comment = React.memo(function({ info, isAncestor, setPostResolved, setPost
                     <img width="40" className="rounded-full" src={ 
                             info.anonymous || userDeleted ? 
                             "/profile-button-img.png" : info.avatarUrl }/>
-                    <div className='flex items-center justify-center'>
-                        { endorsed && 
-                        <span className="h-[18px] mt-2" >
-                            <img src="/endorsed.png" width="18"/>
-                        </span> }
-                        { (commentResolving || commentIsAnswer) &&
-                        <img className="mt-1.5" width="20" src="/checkmark.png" />}
+                    <div className='flex-col items-center justify-center my-2'>
+                        { (commentResolving || commentIsAnswer) && 
+                         <img className="ml-[1px]" width="19" src="/checkmark.png" /> }
+                        { endorsed &&
+                        <>
+                            {(commentResolving || commentIsAnswer) && <div className='h-[0.5rem]'/> }
+                            <span className="h-[20px] w-[21px]" >
+                                <img src="/endorsed.png" width="20"/>
+                            </span>
+                        </>
+                        }
                     </div>
                 </div>
                 <div data-testid="comment-content-container" 
