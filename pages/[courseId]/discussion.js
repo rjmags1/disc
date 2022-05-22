3e6057afed16f8f08e88ced62279d576dd3e775d
RJ Maganini <maganinirj@gmail.com>
2022-05-22 15:06:21 -0400

    minor refactor, add comments, fix unhighlight listing effect bug after post delete

--- pages/[courseId]/discussion.js
+++ pages/[courseId]/discussion.js
@@ -2,16 +2,21 @@ import Layout from '../../components/layout/Layout'
 import CategoryPane from '../../components/discussion/categoryPane/CategoryPane'
 import PostListingsPane from '../../components/discussion/postListingsPane/PostListingsPane'
 import Post from '../../components/discussion/post/Post'
-import { useRouter } from 'next/router'
 import Head from 'next/head'
-import React, { useEffect, useRef, useState } from 'react'
-import { useUser, useCourse } from '../../lib/hooks'
-import 'quill/dist/quill.snow.css'
-import dynamic from 'next/dynamic'
 import NewPost from '../../components/discussion/newPost/NewPost'
-const Editor = dynamic(
+import dynamic from 'next/dynamic'
+const Editor = dynamic( 
+    // must be loaded dynamically client side due to quilljs dependency.
+    // quilljs only works in the browser, requires window.document.
+    // component loaded here and stored in context so it is only loaded over
+    // network once and can be used as needed by discussion page children 
     () => import('../../components/discussion/post/Editor'), 
     { ssr: false })
+import 'quill/dist/quill.snow.css'
+
+import { useRouter } from 'next/router'
+import React, { useEffect, useRef, useState } from 'react'
+import { useUser, useCourse } from '../../lib/hooks'
 
 export const PostContext = React.createContext(null)
 export const TimeContext = React.createContext(null)
@@ -20,24 +25,50 @@ export const EditorContext = React.createContext(null)
 
 function Discussion() {
     const router = useRouter()
+    const { courseId } = router.query
     const catPaneRef = useRef(null)
+
     const [categoryFilter, setCategoryFilter] = useState(new Set())
-    const [showHiddenPane, setShowHiddenPane] = useState(false)
-    const [currentPost, setCurrentPost] = useState(null)
-    const [postListings, setPostListings] = useState([])
-    const [specialListings, setSpecialListings] = useState(
-        { pinned: [], announcements: [] })
-    const [initialLoadTime] = useState(Date.now())
-    const [newPost, setNewPost] = useState(false)
+    const [showHiddenPane, setShowHiddenPane] = (
+        useState(false)
+    ) // controls cat pane visibility via hamburger btn on smaller viewports
+    const [currentPost, setCurrentPost] = (
+        useState(null)
+    ) // set on listing click, is dynamic context
+    const [postListings, setPostListings] = (
+        useState([])) // set on new post or edit post, is dynamic context 
+    const [specialListings, setSpecialListings] = useState({ 
+        pinned: [],
+        announcements: [] 
+    }) // set on new pinned or announced post, is dynamic context
+    const [initialLoadTime] = useState(
+        Date.now()) // syncs displayed data across all loads
+    const [newPost, setNewPost] = (
+        useState(false)
+    ) // used to toggle post display or new post form in post content section
 
-    const postContext = { currentPost, setCurrentPost }
-    const postListingsContext = { 
-        postListings, setPostListings, specialListings, setSpecialListings }
+
+    const postContext = { 
+        currentPost, setCurrentPost 
+    } // dynamic context holds info about post displayed in post content section
+    const postListingsContext = {
+        postListings, setPostListings, specialListings, setSpecialListings 
+    } // dynamic context holding info about posts listed in post listings pane
         
-    const { user, loading: loadingUser } = useUser({ redirectTo: '/login' })
-    const { course, loading: loadingCourse } = useCourse(router.query.courseId)
 
-    useEffect(() => {
+    const {
+        user, loading: loadingUser 
+    } = useUser({ redirectTo: '/login' }) // auth guard
+    const { 
+        course, loading: loadingCourse 
+    } = useCourse(courseId) // get course info for page header
+
+
+    useEffect(() => { 
+        // effect for controlling visibility of category pane when 
+        // viewport is small enough that category pane is hidden by default.
+        // in such a viewport, post listings pane has hamburger btn for 
+        // displaying category pane that when clicked sets showHiddenPane
         if (!catPaneRef.current) return
 
         catPaneRef.current.style.display = showHiddenPane ? "flex" : ""
@@ -49,18 +80,24 @@ function Discussion() {
     }, [catPaneRef, showHiddenPane])
     
     useEffect(() => {
+        // effect for unhighlighting the previous current post after user
+        // setsCurrentPost by clicking another post listing
+
+        if (currentPost === null) return // null when post is deleted
+
         const listingNodes = document.querySelectorAll(
             '[data-testid=post-info-container]')
         for (const listingNode of listingNodes) {
             const splitNodePostId = listingNode.id.split('-')
-            const nodePostId = parseInt(splitNodePostId[splitNodePostId.length - 1])
+            const nodePostId = parseInt(
+                splitNodePostId[splitNodePostId.length - 1])
             if (nodePostId !== currentPost.postId) {
                 listingNode.style.backgroundColor = ""
             }
         }
     }, [currentPost])
 
-    const changeFilter = (add, category) => {
+    const changeCategoryFilter = (add, category) => {
         const newFilter = new Set(categoryFilter)
         if (add) newFilter.add(category)
         else newFilter.delete(category)
@@ -76,28 +113,31 @@ function Discussion() {
         <>
             <Head><title>{ title }</title></Head>
             <EditorContext.Provider value={ Editor } >
-                <TimeContext.Provider value={ initialLoadTime } >
-                    <div data-testid="discussion-container" 
-                    className="flex h-[calc(100vh-48px)] w-full">
-                        <CategoryPane catPaneRef={ catPaneRef } 
-                            changeCategoryFilter={ changeFilter }
-                            newPost={ newPost } setNewPost={ setNewPost } />
-                        <PostListingsContext.Provider 
-                            value={ postListingsContext } >
-                            <PostContext.Provider value={ postContext }>
-                                <section className="flex-auto text-white flex w-full" 
-                                    data-testid="posts-section">
-                                    <PostListingsPane catPaneRef={ catPaneRef }
-                                        toggleCatPane={ () => setShowHiddenPane(!showHiddenPane) }
-                                        categoryFilter={ categoryFilter } setCurrentPost={ setCurrentPost }/>
-                                    { newPost ? 
-                                    <NewPost exitNewPost={ () => setNewPost(false) } /> 
-                                    : <Post /> }
-                                </section>
-                            </PostContext.Provider>
-                        </PostListingsContext.Provider>
-                    </div>
-                </TimeContext.Provider>
+            <TimeContext.Provider value={ initialLoadTime } >
+            <PostListingsContext.Provider value={ postListingsContext } >
+            <PostContext.Provider value={ postContext }>
+
+            <div data-testid="discussion-container" 
+                className="flex h-[calc(100vh-48px)] w-full">
+                <CategoryPane catPaneRef={ catPaneRef } newPost={ newPost }
+                    changeCategoryFilter={ changeCategoryFilter }
+                    setNewPost={ setNewPost } />
+                <section className="flex-auto text-white flex w-full"
+                    data-testid="posts-section">
+                    <PostListingsPane catPaneRef={ catPaneRef }
+                        toggleCatPane={ 
+                            () => setShowHiddenPane(!showHiddenPane) }
+                        categoryFilter={ categoryFilter } 
+                        setCurrentPost={ setCurrentPost }/>
+                    { newPost ? 
+                    <NewPost exitNewPost={ () => setNewPost(false) } /> 
+                    : <Post /> }
+                </section>
+            </div>
+
+            </PostContext.Provider>
+            </PostListingsContext.Provider>
+            </TimeContext.Provider>
             </EditorContext.Provider>
         </>
     )
