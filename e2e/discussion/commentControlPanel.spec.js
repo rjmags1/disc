92a49ab37c046429252e5d1e995c68e26fae046e
RJ Maganini <maganinirj@gmail.com>
2022-06-06 20:04:02 -0400

    small fixes, improve comment nesting depth

--- e2e/discussion/commentControlPanel.spec.js
+++ e2e/discussion/commentControlPanel.spec.js
@@ -13,7 +13,6 @@ const {
     editAndAssertOnEditedPageComment,
     stripTerminatingNewlines,
     dbAssertResetFirstUserAuthoredCommentDeleted,
-    lazyLoadAllTopLevelPageComments,
     dbAssertFirstUserAuthoredPostFirstCommentResAnsDelta,
     assertOnCorrectResolveAnswerButtonLabels,
     clickResolveAnswerButtonUiAssert,
