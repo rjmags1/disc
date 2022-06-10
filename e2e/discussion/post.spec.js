a448bcf7cfa367722d889c2e4031475df2de8fd5
RJ Maganini <maganinirj@gmail.com>
2022-06-09 19:49:29 -0500

    uncomment and remove unnecessary refs

--- e2e/discussion/post.spec.js
+++ e2e/discussion/post.spec.js
@@ -7,10 +7,8 @@ const {
 const { TEST_COURSE_INFO } = require('../lib/course')
 const { 
     assertOnPostContent,
-    assertOnPostControlPanel,
-    TEST_POST_INFO,
+    assertOnPostControlPanelContent,
     dbAssertPostControlPanelBooleanButton,
-    dbAssertResetFirstUserAuthoredCommentDeleted,
     getPostIdFromTitle,
     dbUndeletePost,
     uiAssertPostControlPanelBooleanButton,
@@ -23,7 +21,7 @@ const { getAllNonDeletedDbPostsInPageOrder } = require('../lib/postListings')
 // NOTE: these tests should be run with npm run e2e-serial
 
 test.beforeEach(async ({ page }, { title: testTitle }) => {
-    await login(page, testTitle === 'post content' ? 
+    await login(page, testTitle === 'displayed post content' ? 
         TESTUSER_REGISTERED : TESTUSER_STAFF)
     await expect(page.locator('nav').
         locator('text=/dashboard/i')).toBeVisible()
@@ -40,106 +38,106 @@ test.beforeEach(async ({ page }, { title: testTitle }) => {
         '[data-testid=post-info-container]').nth(0).waitFor()
 })
 
-//test.describe('post content', async () => {
-    //test.slow()
+test.describe('post content', async () => {
+    test.slow()
 
-    //test('displayed post reflects data in db', async ({ page, isMobile }) => {
-        //const postListingLocator = page.locator(
-            //'[data-testid=post-info-container]')
-        //const initialPostListings = await postListingLocator.count()
-        //const allDbPosts = (await getAllNonDeletedDbPostsInPageOrder(
-            //TEST_COURSE_INFO.courseId)).filter(
-                //row => !row.private || row.user_id === TESTUSER_REGISTERED.userId)
-        //for (let i = 0; i < initialPostListings; i++) {
-            //const listingTitle = await page.locator(
-                //'[data-testid=post-info-title]').nth(i).innerText()
-            //await Promise.all([
-                //postListingLocator.nth(i).click(),
-                //page.locator('[data-testid=post-container]', {
-                    //hasText: listingTitle })
-            //])
+    test('displayed post content', async ({ page, isMobile }) => {
+        const postListingLocator = page.locator(
+            '[data-testid=post-info-container]')
+        const initialPostListings = await postListingLocator.count()
+        const allDbPosts = (await getAllNonDeletedDbPostsInPageOrder(
+            TEST_COURSE_INFO.courseId)).filter(
+                row => !row.private || row.user_id === TESTUSER_REGISTERED.userId)
+        for (let i = 0; i < initialPostListings; i++) {
+            const listingTitle = await page.locator(
+                '[data-testid=post-info-title]').nth(i).innerText()
+            await Promise.all([
+                postListingLocator.nth(i).click(),
+                page.locator('[data-testid=post-container]', {
+                    hasText: listingTitle })
+            ])
 
-            //await assertOnPostContent(allDbPosts[i], page)
-            //await assertOnPostControlPanelContent(
-                //allDbPosts[i], page, TESTUSER_REGISTERED.userId)
+            await assertOnPostContent(allDbPosts[i], page)
+            await assertOnPostControlPanelContent(
+                allDbPosts[i], page, TESTUSER_REGISTERED.userId)
 
-            //if (isMobile) await page.locator(
-                //'[data-testid=post-back-btn]').click()
-        //}
-    //})
-//})
+            if (isMobile) await page.locator(
+                '[data-testid=post-back-btn]').click()
+        }
+    })
+})
 
 test.describe('post control panel', async () => {
     test.slow()
 
-    //test('boolean control panel button fxn', async ({ page, isMobile }) => {
-        //// all cp buttons in one post
-        //const postListingLocator = page.locator(
-            //'[data-testid=post-info-container]', {
-                //hasText: TESTUSER_STAFF.fullName }).nth(0)
-        //await Promise.all([
-            //postListingLocator.click(),
-            //expect(page.locator(
-                //"[data-testid=thread-container]").nth(0)).toBeVisible()
-        //])
+    test('boolean control panel button fxn', async ({ page, isMobile }) => {
+        // all cp buttons in one post
+        const postListingLocator = page.locator(
+            '[data-testid=post-info-container]', {
+                hasText: TESTUSER_STAFF.fullName }).nth(0)
+        await Promise.all([
+            postListingLocator.click(),
+            expect(page.locator(
+                "[data-testid=thread-container]").nth(0)).toBeVisible()
+        ])
         
-        //const postTitle = await page.locator(
-            //'[data-testid=post-title]').innerText()
-        //const testPostId = await getPostIdFromTitle(postTitle)
+        const postTitle = await page.locator(
+            '[data-testid=post-title]').innerText()
+        const testPostId = await getPostIdFromTitle(postTitle)
 
-        //const locators = [
-            //page.locator('[data-testid=post-like-button-container]'),
-            //page.locator('[data-testid=post-watch-button-container]'),
-            //page.locator('[data-testid=post-star-button-container]'),
-            //page.locator('[data-testid=post-endorse-button-container]'),
-            //page.locator('[data-testid=post-delete-button-container]')
-        //]
-        //const falseLabels = ['Like', 'Watch', 'Star', 'Endorse', 'Delete']
-        //const trueLabels = ['Unlike', 'Unwatch', 'Unstar', 'Unendorse']
+        const locators = [
+            page.locator('[data-testid=post-like-button-container]'),
+            page.locator('[data-testid=post-watch-button-container]'),
+            page.locator('[data-testid=post-star-button-container]'),
+            page.locator('[data-testid=post-endorse-button-container]'),
+            page.locator('[data-testid=post-delete-button-container]')
+        ]
+        const falseLabels = ['Like', 'Watch', 'Star', 'Endorse', 'Delete']
+        const trueLabels = ['Unlike', 'Unwatch', 'Unstar', 'Unendorse']
         
-        //for (let i = 0; i < locators.length; i++) {
-            //const locator = locators[i]
-            //const label = await locator.innerText()
-            //const preClickUiStatus = !(new RegExp(falseLabels[i]).test(label))
-            //await dbAssertPostControlPanelBooleanButton(
-                //falseLabels[i], preClickUiStatus, 
-                //testPostId, TESTUSER_STAFF.userId)
-            //const preClickLikes = await uiAssertPostControlPanelBooleanButton(
-                //falseLabels[i], preClickUiStatus, 
-                //postListingLocator, postTitle, isMobile, page)
+        for (let i = 0; i < locators.length; i++) {
+            const locator = locators[i]
+            const label = await locator.innerText()
+            const preClickUiStatus = !(new RegExp(falseLabels[i]).test(label))
+            await dbAssertPostControlPanelBooleanButton(
+                falseLabels[i], preClickUiStatus, 
+                testPostId, TESTUSER_STAFF.userId)
+            const preClickLikes = await uiAssertPostControlPanelBooleanButton(
+                falseLabels[i], preClickUiStatus, 
+                postListingLocator, postTitle, isMobile, page)
 
-            //await locator.click()
-            //if (falseLabels[i] === 'Delete') {
-                //let visStatus = true
-                //while (visStatus) {
-                    //visStatus = await page.locator(
-                        //'[data-testid=post-content-container]').isVisible()
-                //}
-            //}
-            //else {
-                //const newLabel = await locator.innerText()
-                //expect(newLabel).toMatch(new RegExp(preClickUiStatus ?
-                    //falseLabels[i] : trueLabels[i]))
-            //}            
-            //await new Promise(res => setTimeout(res, 300))
-            //const postClickLikes = await uiAssertPostControlPanelBooleanButton(
-                //falseLabels[i], !preClickUiStatus, 
-                //postListingLocator, postTitle, isMobile, page)
-            //if (falseLabels[i] === 'Like') {
-                //const postClickDiff = preClickUiStatus ? -1 : 1
-                //expect(postClickLikes - preClickLikes).toBe(postClickDiff)
-            //}
-            //await dbAssertPostControlPanelBooleanButton(
-                //falseLabels[i], !preClickUiStatus, 
-                //testPostId, TESTUSER_STAFF.userId)
+            await locator.click()
+            if (falseLabels[i] === 'Delete') {
+                let visStatus = true
+                while (visStatus) {
+                    visStatus = await page.locator(
+                        '[data-testid=post-content-container]').isVisible()
+                }
+            }
+            else {
+                const newLabel = await locator.innerText()
+                expect(newLabel).toMatch(new RegExp(preClickUiStatus ?
+                    falseLabels[i] : trueLabels[i]))
+            }            
+            await new Promise(res => setTimeout(res, 300))
+            const postClickLikes = await uiAssertPostControlPanelBooleanButton(
+                falseLabels[i], !preClickUiStatus, 
+                postListingLocator, postTitle, isMobile, page)
+            if (falseLabels[i] === 'Like') {
+                const postClickDiff = preClickUiStatus ? -1 : 1
+                expect(postClickLikes - preClickLikes).toBe(postClickDiff)
+            }
+            await dbAssertPostControlPanelBooleanButton(
+                falseLabels[i], !preClickUiStatus, 
+                testPostId, TESTUSER_STAFF.userId)
 
-            //// reset
-            //if (falseLabels[i] === 'Delete') {
-                //await dbUndeletePost(testPostId)
-            //}
-            //else await locator.click()
-        //}
-    //})
+            // reset
+            if (falseLabels[i] === 'Delete') {
+                await dbUndeletePost(testPostId)
+            }
+            else await locator.click()
+        }
+    })
 
     test('edit button control panel fxn', async ({ page }) => {
         const postListingLocator = page.locator(
