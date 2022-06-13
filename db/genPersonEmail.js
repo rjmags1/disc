f9f25a6c46ef1153340cfe5d1454a3309cc948ea
RJ Maganini <maganinirj@gmail.com>
2022-06-13 10:26:36 -0500

    dedupe env files

--- db/genPersonEmail.js
+++ db/genPersonEmail.js
@@ -2,7 +2,7 @@ const { faker } = require('@faker-js/faker')
 const bcrypt = require('bcrypt')
 const path = require('path')
 require('dotenv').config({
-    path: path.resolve(__dirname, './.env.db')
+    path: path.resolve(__dirname, '../.env.local')
 })
 const { Pool } = require("pg")
 const pool = new Pool()
--- e2e.setup.js
+++ e2e.setup.js
@@ -2,6 +2,6 @@ const path = require('path')
 
 module.exports = async () => {
     require('dotenv').config({
-        path: path.resolve(__dirname, './.env.e2e')
+        path: path.resolve(__dirname, './.env.local')
     })
 }
